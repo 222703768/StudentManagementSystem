23a3637c4c15a96d77e3d712c525581051817953
Nondumiso Ngobese <222703768@mycput.ac.za>
2026-02-16 22:47:10 +0200

    finished project

--- src/main/java/za/ac/cput/domain/GraduateStudent.java
+++ src/main/java/za/ac/cput/domain/GraduateStudent.java
@@ -1,15 +1,76 @@
 package za.ac.cput.domain;
 
-public abstract class GraduateStudent {
-    protected String studentId;
-    protected String name;
-    protected String email;
-    protected String department;
+public class GraduateStudent extends Student {
 
-    public abstract double calculateTuition();
+    private boolean researchAssistant;
+    private double stipend;
 
-    public abstract String getStudentType();
+    private GraduateStudent(Builder builder) {
+        this.studentId = builder.studentId;
+        this.name = builder.name;
+        this.email =builder.email;
+        this.department = builder.department;
+        this.researchAssistant = builder.researchAssistant;
+        this.stipend = builder.stipend;
+    }
 
+    @Override
+    public double calculateTuition() {
+        double baseTuition = 5000;
+        if (researchAssistant) {
+            return baseTuition - stipend;
+        }
+        return baseTuition;
+    }
+
+    @Override
+    public String getStudentType() {
+        return "Graduate Student";
+    }
+
+    @Override
     public void displayStudentDetails() {
+        System.out.println("----- Graduate Student -----");
+        System.out.println("ID: " + studentId);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Department: " + department);
+        System.out.println("Research Assistant: " + researchAssistant);
+        System.out.println("Stipend: " + stipend);
+        System.out.println("Tuition: " + calculateTuition());
+        System.out.println();
+    }
+
+    // Builder Class
+    public static class Builder {
+        private String studentId;
+        private String name;
+        private String email;
+        private String department;
+        private boolean researchAssistant;
+        private double stipend;
+
+        public Builder(String studentId, String name, String email, String department) {
+            this.studentId = studentId;
+            this.name = name;
+            this.email = email;
+            this.department = department;
+        }
+
+        public Builder researchAssistant(boolean researchAssistant) {
+            this.researchAssistant = researchAssistant;
+            return this;
+        }
+
+        public Builder stipend(double stipend) {
+            this.stipend = stipend;
+            return this;
+        }
+
+        public GraduateStudent build() {
+            return new GraduateStudent(this);
+        }
     }
 }
+
+
--- src/main/java/za/ac/cput/domain/Student.java
+++ src/main/java/za/ac/cput/domain/Student.java
@@ -13,37 +13,4 @@ package za.ac.cput.domain;
         public void displayStudentDetails() {
         }
 
-        public Student(String studentId, String name, String email, String department) {
-            this.studentId = studentId;
-            this.name = name;
-            this.email = email;
-            this.department = department;
-        }
-
-        public String getStudentId() {
-            return studentId;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public String getDepartment() {
-            return department;
-        }
-
-        @Override
-        public String toString() {
-            return "Student{" +
-                    "studentId='" + studentId + '\'' +
-                    ", name='" + name + '\'' +
-                    ", email='" + email + '\'' +
-                    ", department='" + department + '\'' +
-                    '}';
-        }
-
     }
